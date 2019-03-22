ebd61e17ef25e1dfff613e865fbef829b49fda06
nicknotj <nicknotjustin@gmail.com>
2019-03-22 09:51:15 -0500

    Prettified the logout and login buttons. Username and password input fields now match the word input fields. FAQ is huge.

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -25,14 +25,14 @@ export class LandingPage extends React.Component {
             <p>For New Users:</p>
             <Link to="/register">Register</Link>
             <label>For Confused Users:</label>
-            <button className="faqButton'">FAQ</button>
+            <button className="faqButton">FAQ</button>
         </div>
         <div role="container" className="informercial">
           <h3>Learn Portuguese the RIGHT Way</h3>
           <p>Portugueuse Palavras uses Spaced Reptition,</p>
           <p>focusing on what you don't know and not wasting</p>
           <p>time with what you do. Click on the Register link</p>
-          <p>to learn South America's Biggest Language today!</p>
+          <p>to learn South America's Most Popular Language today!</p>
         </div>
       </div>
     );
